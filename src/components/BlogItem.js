8cb26b21cd91e96346abf792d1e38461711a9a49
Kostiantyn Sukhykh <konstantin.suhih@gmail.com>
2023-01-12 22:57:02 +0200

    123

--- src/components/BlogItem.js
+++ src/components/BlogItem.js
@@ -1,7 +1,7 @@
 import '../assets/scss/BlogItem.scss';
 
 function BlogItem() {
-	return <div className='blog-item__wrapper'>Lorem ipsum</div>;
+	return <div className='blog-item__wrapper'>One more Lorem ipsum</div>;
 }
 
 export default BlogItem;
